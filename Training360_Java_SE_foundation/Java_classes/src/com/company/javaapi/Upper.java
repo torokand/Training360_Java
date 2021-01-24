bb3b34dc61e9266b675341371ee1356a5d531950
torokand <torok.andras@hotmail.com>
2021-01-24 12:08:16 +0100

    jar file has fininshed.

---
+++ Training360_Java_SE_foundation/Java_classes/src/com/company/javaapi/Upper.java
@@ -0,0 +1,9 @@
+package com.company.javaapi;
+
+public class Upper {
+    public static void main(String[] args) {
+        String s = "Hello World!";
+        System.out.println(s.toUpperCase());
+    }
+
+}
